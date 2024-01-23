afd56db3f09cdf5554d840658bc0e541c008cc94
Kelvin <kelvi_cms@hotmail.com>
2024-01-23 06:29:56 -0300

    updating the ZigZagConversion docs

--- ZigZagConversion/ZigZagConversion.java
+++ ZigZagConversion/ZigZagConversion.java
@@ -1,3 +1,21 @@
+/**
+ * @author Kelvin Clovis Montoli de Souza
+ *
+ * LeetCode Question: Zigzag Conversion	
+ * Problem Statement: The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: 
+ * P   A   H   N
+ * A P L S I I G
+ * Y   I   R
+ * And then read line by line: "PAHNAPLSIIGYIR"
+ * Write the code that will take a string and make this conversion given a number of rows.
+ *
+ * Solution Approach: Traverse the string and append characters to the corresponding row in a StringBuilder.
+ * 
+ * This solution comes from a Youtuber named NeetCode (https://www.youtube.com/@NeetCode) , this is just a Java Implementation of his solution
+ * Source: https://www.youtube.com/watch?v=Q2Tw6gcVEwc
+ * 
+ */
+
 package LeetCode.ZigZagConversion;
 
 import java.util.ArrayList;
