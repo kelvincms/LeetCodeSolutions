57ba8d256466f871c7870141647dacf862ee866c
Kelvin Clovis <kelvin_cms@hotmail.com>
2024-02-03 11:29:21 -0300

    Adding the solution for the 'IsSubsequence' problem

---
+++ IsSubsequence/IsSubsequence.java
@@ -0,0 +1,42 @@
+/**
+ * @author Kelvin Clovis Montoli de Souza
+ *
+ * LeetCode Question: IsSubsequence
+ * Problem Statement: Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+ * A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) 
+ * of the characters without disturbing the relative positions of the remaining characters. 
+ * (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+ *
+ * Solution Approach: Checking for subsequence by iterating through the characters of both strings, 
+ * we start both of the indexes at 0 and when the subsequenceIndex 
+ * reach the length of the substring itself we simply return the result.
+ */
+
+package LeetCode.IsSubsequence;
+
+public class IsSubsequence {
+
+    public static boolean isSubsequence(String s, String t) {
+
+        if (s.length() == 0 && t.length() == 0) {
+            return true;
+        } else if (s.length() == 0) {
+            return false;
+        } else if (t.length() == 0) {
+            return true;
+        }
+
+        for (int index = 0, subsequenceIndex = 0; index < s.length(); index++) {
+
+            if (s.charAt(index) == t.charAt(subsequenceIndex)) {
+                subsequenceIndex++;
+                if (subsequenceIndex == t.length()) {
+                    return true;
+                }
+            }
+
+        }
+
+        return false;
+    }
+}
---
+++ IsSubsequence/IsSubsequenceTest.java
@@ -0,0 +1,73 @@
+/**
+ * @author Kelvin Clovis Montoli de Souza
+ *
+ * Unit tests for the IsSubsequence class.
+ */
+
+package LeetCode.IsSubsequence;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class IsSubsequenceTest {
+
+    @Test
+    public void isSubsequence_emptyInputStringWithNonEmptySubSequence_expectedAnswer() {
+        // Test case for an empty input string with a nont empty Subsequence string
+        String input = "", subsequenceSring = "abbcbc";
+        boolean answer, expectedAnswer = false;
+        answer = IsSubsequence.isSubsequence(input, subsequenceSring);
+        assertEquals(expectedAnswer, answer);
+    }
+
+    @Test
+    public void isSubsequence_emptyInputStringWithEmptySubSequence_expectedAnswer() {
+        // Test case for an empty string for both input and subsequence
+        String input = "", subsequenceSring = "";
+        boolean answer, expectedAnswer = true;
+        answer = IsSubsequence.isSubsequence(input, subsequenceSring);
+        assertEquals(expectedAnswer, answer);
+    }
+
+    @Test
+    public void isSubsequence_baseCase_expectedAnswer() {
+        // Test case for the base case of the problem
+        String input = "afdwdwgfefqewqewqtrrraewweabfd", subsequenceSring = "awwabd";
+        boolean answer, expectedAnswer = true;
+        answer = IsSubsequence.isSubsequence(input, subsequenceSring);
+        assertEquals(expectedAnswer, answer);
+    }
+
+    @Test
+    public void isSubsequence_noSubSequence_expectedAnswer() {
+        // Test case for a solution that returns false because there's no subsequence
+        // string
+
+        String input = "afdwdwgfefqewqewqtrrraewweabfd", subsequenceSring = "aaaaaaaaa";
+        boolean answer, expectedAnswer = false;
+        answer = IsSubsequence.isSubsequence(input, subsequenceSring);
+        assertEquals(expectedAnswer, answer);
+    }
+
+    @Test
+    public void isSubsequence_emptySubSequence_expectedAnswer() {
+        // Test case for an input with an emptySubstring
+
+        String input = "afdwdwgfe  fqe wqewq  trrraeww    eabfd", subsequenceSring = "";
+        boolean answer, expectedAnswer = true;
+        answer = IsSubsequence.isSubsequence(input, subsequenceSring);
+        assertEquals(expectedAnswer, answer);
+    }
+
+    @Test
+    public void isSubsequence_subSequenceGreaterThanString_expectedAnswer() {
+        // Test case for an input where the subsequence is larger than the input
+
+        String input = "abc", subsequenceSring = "ahbgdc";
+        boolean answer, expectedAnswer = false;
+        answer = IsSubsequence.isSubsequence(input, subsequenceSring);
+        assertEquals(expectedAnswer, answer);
+    }
+
+}
