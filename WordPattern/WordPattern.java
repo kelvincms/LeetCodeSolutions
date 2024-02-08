fa9de6b9180786d542e341f33d0733a012a15ea5
Kelvin Clovis <kelvin_cms@hotmail.com>
2024-02-07 23:01:19 -0300

    Adding the solution for the WordPattern problem

---
+++ WordPattern/WordPattern.java
@@ -0,0 +1,54 @@
+/**
+ * @author Kelvin Clovis Montoli de Souza
+ *
+ * LeetCode Question: 290. Word Pattern
+ * Problem Statement: Given a pattern and a string s, find if s follows the same pattern.
+ *
+ * Solution Approach: Split the string into words and use a HashMap to map characters in the pattern to words,
+ * ensuring that each character is consistently mapped to the same word and vice versa.
+ */
+
+
+package LeetCode.WordPattern;
+
+import java.util.HashMap;
+
+public class WordPattern {
+
+    public static boolean wordPattern(String pattern, String s) {
+
+        if (pattern.length() == 0 && s.length() == 0) {
+            return true;
+        }
+
+        if (pattern.length() == 0 || s.length() == 0) {
+            return false;
+        }
+
+        String[] words = s.split(" ");
+
+        if (pattern.length() != words.length) {
+            return false;
+        }
+
+        HashMap<Character, String> patternMap = new HashMap<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char currentChar = pattern.charAt(i);
+            String currentWord = words[i];
+
+            if (patternMap.containsKey(currentChar)) {
+                if (!patternMap.get(currentChar).equals(currentWord)) {
+                    return false;
+                }
+            } else {
+                if (patternMap.containsValue(currentWord)) {
+                    return false;
+                }
+                patternMap.put(currentChar, currentWord);
+            }
+        }
+
+        return true;
+    }
+}
---
+++ WordPattern/WordPatternTest.java
@@ -0,0 +1,64 @@
+/**
+ * @author Kelvin Clovis Montoli de Souza
+ *
+ * Unit tests for the WordPattern class.
+ */
+
+package LeetCode.WordPattern;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class WordPatternTest {
+
+    @Test
+    public void wordPattern_emptyInputStrings_expectTrue() {
+        // Unit test for empty input strings
+        String pattern = "", input = "";
+
+        boolean answer, expectedAnswer = true;
+        answer = WordPattern.wordPattern(pattern, input);
+        assertEquals("Expected answer is diffrent from the actual answer", expectedAnswer, answer);
+    }
+
+    @Test
+    public void wordPattern_emptyPatternString_expectFalse() {
+        // Unit test for an empty "pattern" input
+        String pattern = "", input = "aaaa dddd sss www";
+        boolean answer, expectedAnswer = false;
+        answer = WordPattern.wordPattern(pattern, input);
+        assertEquals("Expected answer is diffrent from the actual answer", expectedAnswer, answer);
+    }
+
+    @Test
+    public void wordPattern_emptyinputString_expectFalse() {
+        // Unit test for an empty "input"
+        String pattern = "aaddwwe", input = "";
+        boolean answer, expectedAnswer = false;
+        answer = WordPattern.wordPattern(pattern, input);
+        assertEquals("Expected answer is diffrent from the actual answer", expectedAnswer, answer);
+    }
+
+    @Test
+    public void wordPattern_baseCase_expectTrue() {
+        // Unit test for the base case of the application when it should succed
+
+        String pattern = "adde", input = "note long long separated";
+        boolean answer, expectedAnswer = true;
+        answer = WordPattern.wordPattern(pattern, input);
+        assertEquals("Expected answer is diffrent from the actual answer", expectedAnswer, answer);
+
+    }
+
+    @Test
+    public void wordPattern_baseCase_expectFalse() {
+        // Unit test for the base case of the application when it should fail
+
+        String pattern = "adde", input = "note long long long";
+        boolean answer, expectedAnswer = false;
+        answer = WordPattern.wordPattern(pattern, input);
+        assertEquals("Expected answer is diffrent from the actual answer", expectedAnswer, answer);
+    }
+
+}
